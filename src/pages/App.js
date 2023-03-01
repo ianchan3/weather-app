9c7d9c1332dfe205c9fa32a49727e94f7bea3597
ianchan3 <ianchan3138@gmail.com>
2023-03-01 11:17:56 -0800

    Fetch API Weather Information Render

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,40 +1,58 @@
 import './App.css';
 import { useState } from 'react';
 
+const api = {
+  key: "3db5c4a90aa425763a6b2d8a10469531",
+  base: "https://api.openweathermap.org/data/2.5/",
+}
+
 export default function App() {
 
   const [input, setInput] = useState('');
   const [weather, setWeather] = useState({});
 
-  const api = {
-    key: "3db5c4a90aa425763a6b2d8a10469531",
-    base: "https://api.openweathermap.org/data/2.5/",
+  const fullDate = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
   }
 
 const handleSubmit = (e) => {
-  fetch(`${api.base}weather?q=${input}&units=metric&appid=${api.key}`) 
-  .then(response => response.json())
-  .then(result => {
-    setWeather(result);
-    console.log(result);
-    setInput("");
-  })
+  if (e.key == "Enter" || e.type == "click" ) {
+    fetch(`${api.base}weather?q=${input}&units=imperial&appid=${api.key}`) 
+    .then(response => response.json())
+    .then(result => {
+      setWeather(result);
+      console.log(result);
+      setInput("");
+    })
+  }
 }
-
   return (
     <div className="App">
       <span className='details'>
-        <div>
+        <div className="weather-form">
           <h1>Weather App</h1>
-          <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
+          <input type="text" value={input} onChange={(e) => setInput(e.target.value)} onKeyPress={handleSubmit}  />
           <button onClick={handleSubmit}>Submit</button>
         </div>
-        <div>
-        <h1>{ weather.name }</h1>
-
+        {typeof weather.main != 'undefined' ? (
+          <div>
+            <h1>Date: { fullDate( new Date()) }</h1>
+            <h1>City: {`${ weather.name }, ${ weather.sys.country }`}</h1>
+            <h1>Temperature: {` ${ weather.main.temp }°` }</h1>
+            <h1>Wind: { weather.wind.speed } mph</h1>
+            <h1>Description: { weather.weather[0].description }</h1>
+          </div>
+        ) : ("")}
+        </span>
         </div>
-      </span>
-    </div>
   );
 }
 
